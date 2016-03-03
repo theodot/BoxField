dceb6a5ab8373bbd206ce4cb2d993da83808c7ed
Mr. Theodoropoulos <tony.theodoropoulos@ed.amdsb.ca>
2016-03-03 08:28:57 -0500

    Initial Commit

---
+++ BoxField/Screens/GameScreen.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BoxField
+{
+    public partial class GameScreen : UserControl
+    {
+        //player1 button control keys - DO NOT CHANGE
+        Boolean leftArrowDown, downArrowDown, rightArrowDown, upArrowDown, bDown, nDown, mDown, spaceDown;
+
+        //used to draw boxes on screen
+        SolidBrush boxBrush = new SolidBrush(Color.White);
+
+        //TODO - create a list of Boxes
+
+        public GameScreen()
+        {
+            InitializeComponent();
+        }
+
+        private void GameScreen_Load(object sender, EventArgs e)
+        {
+            //TODO - create initial box object and add it to list of Boxes
+        }
+
+        private void GameScreen_PreviewKeyDown(object sender, PreviewKeyDownEventArgs e)
+        {
+            //player 1 button presses
+            switch (e.KeyCode)
+            {
+                case Keys.Left:
+                    leftArrowDown = true;
+                    break;
+                case Keys.Down:
+                    downArrowDown = true;
+                    break;
+                case Keys.Right:
+                    rightArrowDown = true;
+                    break;
+                case Keys.Up:
+                    upArrowDown = true;
+                    break;
+                case Keys.B:
+                    bDown = true;
+                    break;
+                case Keys.N:
+                    nDown = true;
+                    break;
+                case Keys.M:
+                    mDown = true;
+                    break;
+                case Keys.Space:
+                    spaceDown = true;
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void GameScreen_KeyUp(object sender, KeyEventArgs e)
+        {
+            //player 1 button releases
+            switch (e.KeyCode)
+            {
+                case Keys.Left:
+                    leftArrowDown = false;
+                    break;
+                case Keys.Down:
+                    downArrowDown = false;
+                    break;
+                case Keys.Right:
+                    rightArrowDown = false;
+                    break;
+                case Keys.Up:
+                    upArrowDown = false;
+                    break;
+                case Keys.B:
+                    bDown = false;
+                    break;
+                case Keys.N:
+                    nDown = false;
+                    break;
+                case Keys.M:
+                    mDown = false;
+                    break;
+                case Keys.Space:
+                    spaceDown = false;
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void gameLoop_Tick(object sender, EventArgs e)
+        {
+            //TODO - update position of each box
+
+            //TODO - remove box from list if it is off screen
+
+            Refresh();
+        }
+
+        private void GameScreen_Paint(object sender, PaintEventArgs e)
+        {
+            //TODO - draw each box to the screen
+        }
+
+
+    }
+}
